1a7cc8304c7eec8101e9d688f8cf8b43191e3f36
Marvinj10 <jamesmarvin39@gmail.com>
2018-08-22 18:14:47 +0300

    Added the Role belongs to User relationship

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'role_id','is_active','name', 'email', 'password',
     ];
 
     /**
@@ -23,4 +23,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //Relationship to the Role--The user belongs to the role
+    public function roles(){
+        return $this->belongsTo('App\Role');
+    }
+
 }
